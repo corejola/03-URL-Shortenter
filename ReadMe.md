dc238d6267d867ce14134a29078c747ecaff0f62
Cedric Orejola <cedricorejola@gmail.com>
2020-10-23 14:10:02 -0700

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+# URL - Shortener
+
+1. Utilizing  ShortId npm package to generate a short URL
+2. Saving and linking the Original Long url to the short url with a simple MongoDB - Atlas database
+3. Utilizing a simple handlbars html interface to manage the views from express and display the URLs
+
+### Assumtions
+1. UI accepts a long url and auto generates a short url
+2. long & short url pair saved to a DB
+3. short url provides access to long url
+
+### Limitations
+4. needed the database relationship to provide the link & redirect
+
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const connectDB = require('./config/db')
+const exphbs = require('express-handlebars');
+const Url = require('./models/Url')
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.urlencoded({ extended: false }))
+
+// Connect to Database
+connectDB();
+
+app.use(express.json({ extended: false }))
+
+// post request
+app.post('/shorten', async (req, res) => {
+    await Url.create({
+        longUrl: req.body.fullUrl
+    })
+    res.redirect('/')
+})
+
+// define views & get request
+app.get('/', async (req, res) => {
+    let shortUrls = await Url.find().lean()
+    let data = []
+    shortUrls.forEach(url => {
+        data.push(url)
+    })
+
+    res.render("home", { urls: data })
+})
+
+// get request based on shortUrl Params & redirect
+app.get('/:shortUrl', async (req, res) => {
+    const shortenedUrl = await Url.findOne({ shortUrl: req.params.shortUrl })
+    if (shortenedUrl == null) return res.sendStatus(404)
+    console.log(shortenedUrl)
+    shortenedUrl.save()
+
+    res.redirect(shortenedUrl.longUrl)
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
